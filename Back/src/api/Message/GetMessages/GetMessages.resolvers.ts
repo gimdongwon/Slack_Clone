501bc3e2f88db79728a2de5fa3bd5258bf95ec9c
kimdongwon <ehddnjs8989@gmail.com>
2021-01-16 16:30:07 +0900

    [complete] GetMessages query를 잘못 날렸었음

--- Back/src/api/Message/GetMessages/GetMessages.resolvers.ts
+++ Back/src/api/Message/GetMessages/GetMessages.resolvers.ts
@@ -1,3 +1,7 @@
+import {
+  GetMessagesQueryArgs,
+  GetMessagesResponse
+} from "./../../../types/graphql";
 import { Resolvers } from "src/types/resolvers";
 
 // 상대경로로 사용해야함 (절대경로로는 파일을 못찾음)
@@ -6,7 +10,10 @@ import Message from "../../../../src/entities/Message";
 // 아래 쿼리 구조는 graphQL에서 정해놓은 규격
 const resolvers: Resolvers = {
   Query: {
-    GetMessages: async (_, args) => {
+    GetMessages: async (
+      _,
+      args: GetMessagesQueryArgs
+    ): Promise<GetMessagesResponse> => {
       // GetMessages 라는 동작에 대한 비즈니스 로직 (Spring의 Controller와 유사)
       // 사용자가 API를 호출했을때 인자값이 args 안으로 들어온다.
 
