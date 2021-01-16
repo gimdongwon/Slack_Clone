2f80da799ca1f4b5b24f0fd2873d0c3613520dde
kimdongwon <ehddnjs8989@gmail.com>
2021-01-17 02:29:37 +0900

    [Complete] Front Basic Setting

---
+++ Front/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <>Hi, everyone</>
+    )
+}
+
+export default App;
---
+++ Front/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+
+const appIndex = path.resolve(__dirname, "src", "index.tsx");
+const appBuild = path.resolve(__dirname, "build");
+const appSrc = path.resolve(__dirname, "src");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const appHtml = path.resolve(__dirname, "public", "index.html");
+
+const appPublic = path.resolve(__dirname, "public");
+const shouldUseSourceMap = process.env.GENERATE_SOURCEMAP !== "false";
+
+module.exports = webpackEnv => {
+  const isEnvDevelopment = webpackEnv === "development";
+  const isEnvProduction = webpackEnv === "production";
+  return {
+    mode: webpackEnv,
+    entry: appIndex,
+    output: {
+      path: appBuild,
+      filename: isEnvProduction
+        ? "static/js/[name].[contenthash:8].js"
+        : isEnvDevelopment && "static/js/bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "ts-loader",
+              options: {
+                transpileOnly: isEnvDevelopment ? true : false
+              }
+            }
+          ]
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"]
+    },
+    plugins: [new HtmlWebpackPlugin({ template: appHtml })],
+    devServer: {
+      port: 3000,
+      contentBase: appPublic,
+      open: true,
+      historyApiFallback: true,
+      overlay: true,
+      stats: "errors-warnings"
+    },
+    devtool: isEnvProduction
+      ? shouldUseSourceMap ? "source-map" : false
+      : isEnvDevelopment && "cheap-module-source-map"
+  };
+};
